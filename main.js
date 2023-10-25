2de065d647574b53b5cbdcaab8ee97cb9bb81ecf
brunamartins12 <134522899+brunamartins12@users.noreply.github.com>
2023-10-25 12:11:38 -0300

    up

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ function tocaSomClap(){
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 function tocaSomTim(){
-    document.querySelector('#som_tecla_tim').play();
+        document.querySelector('#som_tecla_tim').play();
 }
 document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
